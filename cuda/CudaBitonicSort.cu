bf9c8caba37beaf692a03ebab919e9ac44072dd6
Curt Bridgers <bridgerspc@appstate.edu>
2022-06-21 16:06:44 +0000

    Got it working for sizes up to 2^11. Beyond that, the sort *very nearly* works, and I don't quite know why yet.

--- cuda/CudaBitonicSort.cu
+++ cuda/CudaBitonicSort.cu
@@ -18,15 +18,24 @@
 #define MAX_THREADS 1024
 
 __global__ void
-d_bitonic_merge_kernel(int *arr, long arr_size, long local_size)
+d_bitonic_merge_kernel(int *arr, long arr_size, long local_size, long step)
 {
     long i;
     long tid = gridDim.x*blockIdx.x+threadIdx.x;
     long start_idx = tid * local_size;
 
+    long aoe = pow(2, step);
+    long tmp = tid % (2*aoe);
+
+    int order;
+    if (tmp < aoe)
+        order = ASCENDING;
+    else
+        order = DESCENDING;
+
     int half = local_size / 2;
     long end_idx = start_idx + half;
-    int order = !(tid % 2);
+    /* int order = !(tid % 2); */
 
     if (start_idx >= arr_size) return;
 #ifdef DEBUG
@@ -84,9 +93,9 @@ void
 bitonic_sort(int *arr, long size)
 {
     long num_elems_per_subarray = 1;
-    long num_subarrays = size / num_elems_per_subarray;
-    long num_threads = num_subarrays / 2;
-    long stage = 1;
+    /* long num_subarrays = size / num_elems_per_subarray; */
+    //long num_threads = num_subarrays / 2;
+    long stage = 0;
 
     // Copying array to cuda device
     int *d_arr;
@@ -96,31 +105,55 @@ bitonic_sort(int *arr, long size)
 
     while (num_elems_per_subarray != size)
     {
-        /* printf("num_elems_per_subarray: %d\nnum_blocks: %d\nnum_threads: %d\n\n", */
-        /*         num_elems_per_subarray, num_subarrays, num_threads); */
 #ifdef DEBUG
         printf("\n~~~~~~~~~~~~~~~~~~~~~~~Stage %ld~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n",
                 stage);
+        printf("num_elems_per_subarray: %ld\nnum_blocks: %ld\nnum_threads: %ld\n\n",
+                num_elems_per_subarray, num_subarrays, num_threads);
 #endif
 
         // Call kernel with grid=1,1,1 block=num_threads,1,1
         // Each thread in the block will have 2 subarrays to merge
-        int num_blocks = SDIV(num_threads, MAX_THREADS);
-        dim3 grid(num_blocks,1,1);
-        dim3 block(MAX_THREADS,1,1);
+        /* int num_blocks = SDIV(num_threads, MAX_THREADS); */
+        /* dim3 grid(num_blocks,1,1); */
+        /* dim3 block(MAX_THREADS,1,1); */
+
+        long step_size = 2*num_elems_per_subarray;
+        long step_num_elems_per_subarray = num_elems_per_subarray;
+        long step_num_subarrays = step_size / num_elems_per_subarray;
+        long step_num_threads = step_num_subarrays / 2;
+
+        long step = 0;
+        while (step_num_elems_per_subarray >= 1) {
 #ifdef DEBUG
-        printf("grid(%d)\tblock(%d)\n", num_blocks, MAX_THREADS);
+            printf("\n~~~~~~~~~~~~~~~~~~~~~~~Step %ld~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n",
+                    step);
+            printf("step_num_elems_per_subarray: %ld\nstep_num_blocks: %ld\nstep_num_threads: %ld\n\n",
+                    step_num_elems_per_subarray, step_num_subarrays, step_num_threads);
 #endif
-        d_bitonic_merge_kernel<<<grid, block>>>(d_arr, size, 2*num_elems_per_subarray);
-        CUERR;
+            int step_num_blocks = SDIV(step_num_threads, MAX_THREADS);
+            dim3 grid(step_num_blocks,1,1);
+            dim3 block(MAX_THREADS,1,1);
+            d_bitonic_merge_kernel<<<grid, block>>>(d_arr, size, step_size, step);
+            CUERR;
+            cudaDeviceSynchronize();
+
+#ifdef DEBUG
+            printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+#endif
+
+            step_num_elems_per_subarray /= 2;
+            step_size /= 2;
+            step++;
+        }
 
 #ifdef DEBUG
         usleep(500000);
 #endif
 
         num_elems_per_subarray *= 2;
-        num_subarrays = size / num_elems_per_subarray;
-        num_threads = num_subarrays / 2;
+        /* num_subarrays = size / num_elems_per_subarray; */
+        /* num_threads = num_subarrays / 2; */
         stage += 1;
     }
 
@@ -141,8 +174,8 @@ init_array(int *arr, const long size) {
     int i;
 
     for (i = 0; i < size; i++) {
-        /* arr[i] = rand() % ARR_MAX_INT; */
-        arr[i] = i;
+        arr[i] = rand() % ARR_MAX_INT;
+        /* arr[i] = i; */
     }
 }
 
@@ -154,16 +187,11 @@ init_array(int *arr, const long size) {
  *         array.
  */
 void
-print_array(int *arr, const long size, char *label) {
+print_array(int *arr, const long size) {
     int i;
 
-    printf("%s:\n[", label);
     for (i = 0; i < size; i++) {
-        if (i == size - 1) {
-            printf("%d]\n", arr[i]);
-        } else {
-            printf("%d, ", arr[i]);
-        }
+        printf("%d\n", arr[i]);
     }
     return;
 }
@@ -201,18 +229,13 @@ main(int argc, char *argv[])
     arr = (int*) malloc(arr_size * sizeof(*arr));
     init_array(arr, arr_size);
 
-#ifdef DEBUG
-    char label1[] = "Before";
-    print_array(arr, arr_size, label1);
-#endif
+    /* char label1[] = "Before"; */
+    /* print_array(arr, arr_size, label1); */
 
     // Perform the sort
     bitonic_sort(arr, arr_size);
 
-#ifdef DEBUG
-    char label2[] = "\nAfter";
-    print_array(arr, arr_size, label2);
-#endif
+    print_array(arr, arr_size);
 
 	return EXIT_SUCCESS;	
 }
